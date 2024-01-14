4d52f6b8c87efe6dbedd20f029b738bbbed4b682
oslim <adewaleselim6@gmail.com>
2024-01-14 20:19:25 +0100

    fix error

--- src/controller/videos.js
+++ src/controller/videos.js
@@ -6,26 +6,26 @@ const UserVideo =  require('../model/UserAdVideos')
 const { Video } = new Mux(process.env.MUX_ACCESS_TOKEN, process.env.MUX_SECRET_KEY);
 
 
-// const createVideo = async (req, res) => {
-//     const { videoUrl } = req.body;
-//     if (!videoUrl) {
-//         return res.status(400).json({ message: 'Video URL is required' });
-//     }
+const createVideo = async (req, res) => {
+    const { videoUrl } = req.body;
+    if (!videoUrl) {
+        return res.status(400).json({ message: 'Video URL is required' });
+    }
 
-//     try {
-//         const asset = await Video.Assets.create({
-//             input: videoUrl,
-//             playback_policy: ['public'] // Set playback policy as needed (public or signed)
-//         });
-//         res.status(201).json({ 
-//             message: 'Video created successfully',
-//             asset: asset 
-//         });
-//     } catch (error) {
-//         console.error('Error creating video in Mux:', error);
-//         res.status(500).json({ message: 'Error creating video' });
-//     }
-// };
+    try {
+        const asset = await Video.Assets.create({
+            input: videoUrl,
+            playback_policy: ['public'] // Set playback policy as needed (public or signed)
+        });
+        res.status(201).json({ 
+            message: 'Video created successfully',
+            asset: asset 
+        });
+    } catch (error) {
+        console.error('Error creating video in Mux:', error);
+        res.status(500).json({ message: 'Error creating video' });
+    }
+};
 
 
 
